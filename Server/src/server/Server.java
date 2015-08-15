b3f5a2ca1930529951c9f6aa1f7d0aa7ed908117
oscar <revolver74>
2015-08-15 19:37:50 +0200

    adeu main

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -11,14 +11,4 @@ package server;
  */
 public class Server {
 
-    
-    
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hola server");
-    }
-    
 }
